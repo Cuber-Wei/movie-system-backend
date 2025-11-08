Cuber-Wei/software-design

优化文件结构

src/main/java/com/l0v3ch4n/oj/controller/PostFavourController.java
@@ -1,0 +1,107 @@
+package com.l0v3ch4n.oj.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.l0v3ch4n.oj.common.BaseResponse;
+import com.l0v3ch4n.oj.common.ErrorCode;
+import com.l0v3ch4n.oj.common.ResultUtils;
+import com.l0v3ch4n.oj.exception.BusinessException;
+import com.l0v3ch4n.oj.exception.ThrowUtils;
+import com.l0v3ch4n.oj.model.dto.post.PostQueryRequest;
+import com.l0v3ch4n.oj.model.dto.postfavour.PostFavourAddRequest;
+import com.l0v3ch4n.oj.model.dto.postfavour.PostFavourQueryRequest;
+import com.l0v3ch4n.oj.model.entity.Post;
+import com.l0v3ch4n.oj.model.entity.User;
+import com.l0v3ch4n.oj.model.vo.PostVO;
+import com.l0v3ch4n.oj.service.PostFavourService;
+import com.l0v3ch4n.oj.service.PostService;
+import com.l0v3ch4n.oj.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 帖子收藏接口
+ */
+@RestController
+@RequestMapping("/post_favour")
+@Slf4j
+public class PostFavourController {
+
+    @Resource
+    private PostFavourService postFavourService;
+
+    @Resource
+    private PostService postService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 收藏 / 取消收藏
+     *
+     * @param postFavourAddRequest
+     * @param request
+     * @return resultNum 收藏变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doPostFavour(@RequestBody PostFavourAddRequest postFavourAddRequest,
+                                              HttpServletRequest request) {
+        if (postFavourAddRequest == null || postFavourAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能操作
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postFavourAddRequest.getPostId();
+        int result = postFavourService.doPostFavour(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+    /**
+     * 获取我收藏的帖子列表
+     *
+     * @param postQueryRequest
+     * @param request
+     */
+    @PostMapping("/my/list/page")
+    public BaseResponse<Page<PostVO>> listMyFavourPostByPage(@RequestBody PostQueryRequest postQueryRequest,
+                                                             HttpServletRequest request) {
+        if (postQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User loginUser = userService.getLoginUser(request);
+        long current = postQueryRequest.getCurrent();
+        long size = postQueryRequest.getPageSize();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        Page<Post> postPage = postFavourService.listFavourPostByPage(new Page<>(current, size),
+                postService.getQueryWrapper(postQueryRequest), loginUser.getId());
+        return ResultUtils.success(postService.getPostVOPage(postPage, request));
+    }
+
+    /**
+     * 获取用户收藏的帖子列表
+     *
+     * @param postFavourQueryRequest
+     * @param request
+     */
+    @PostMapping("/list/page")
+    public BaseResponse<Page<PostVO>> listFavourPostByPage(@RequestBody PostFavourQueryRequest postFavourQueryRequest,
+                                                           HttpServletRequest request) {
+        if (postFavourQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        long current = postFavourQueryRequest.getCurrent();
+        long size = postFavourQueryRequest.getPageSize();
+        Long userId = postFavourQueryRequest.getUserId();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20 || userId == null, ErrorCode.PARAMS_ERROR);
+        Page<Post> postPage = postFavourService.listFavourPostByPage(new Page<>(current, size),
+                postService.getQueryWrapper(postFavourQueryRequest.getPostQueryRequest()), userId);
+        return ResultUtils.success(postService.getPostVOPage(postPage, request));
+    }
+}

src/main/java/com/l0v3ch4n/oj/model/dto/post/PostEsDTO.java
@@ -1,0 +1,119 @@
+package com.l0v3ch4n.oj.model.dto.post;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.json.JSONUtil;
+import com.l0v3ch4n.oj.model.entity.Post;
+import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 帖子 ES 包装类
+ **/
+// todo 取消注释开启 ES（须先配置 ES）
+//@Document(indexName = "post")
+@Data
+public class PostEsDTO implements Serializable {
+
+    private static final String DATE_TIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+
+    /**
+     * id
+     */
+    @Id
+    private Long id;
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 内容
+     */
+    private String content;
+
+    /**
+     * 标签列表
+     */
+    private List<String> tags;
+
+    /**
+     * 点赞数
+     */
+    private Integer thumbNum;
+
+    /**
+     * 收藏数
+     */
+    private Integer favourNum;
+
+    /**
+     * 创建用户 id
+     */
+    private Long userId;
+
+    /**
+     * 创建时间
+     */
+    @Field(index = false, store = true, type = FieldType.Date, format = {}, pattern = DATE_TIME_PATTERN)
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    @Field(index = false, store = true, type = FieldType.Date, format = {}, pattern = DATE_TIME_PATTERN)
+    private Date updateTime;
+
+    /**
+     * 是否删除
+     */
+    private Integer isDelete;
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 对象转包装类
+     *
+     * @param post
+     * @return
+     */
+    public static PostEsDTO objToDto(Post post) {
+        if (post == null) {
+            return null;
+        }
+        PostEsDTO postEsDTO = new PostEsDTO();
+        BeanUtils.copyProperties(post, postEsDTO);
+        String tagsStr = post.getTags();
+        if (StringUtils.isNotBlank(tagsStr)) {
+            postEsDTO.setTags(JSONUtil.toList(tagsStr, String.class));
+        }
+        return postEsDTO;
+    }
+
+    /**
+     * 包装类转对象
+     *
+     * @param postEsDTO
+     * @return
+     */
+    public static Post dtoToObj(PostEsDTO postEsDTO) {
+        if (postEsDTO == null) {
+            return null;
+        }
+        Post post = new Post();
+        BeanUtils.copyProperties(postEsDTO, post);
+        List<String> tagList = postEsDTO.getTags();
+        if (CollUtil.isNotEmpty(tagList)) {
+            post.setTags(JSONUtil.toJsonStr(tagList));
+        }
+        return post;
+    }
+}

src/main/java/com/l0v3ch4n/oj/service/UserService.java
@@ -1,0 +1,119 @@
+package com.l0v3ch4n.oj.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.l0v3ch4n.oj.model.dto.user.UserQueryRequest;
+import com.l0v3ch4n.oj.model.entity.User;
+import com.l0v3ch4n.oj.model.vo.LoginUserVO;
+import com.l0v3ch4n.oj.model.vo.UserVO;
+import me.chanjar.weixin.common.bean.WxOAuth2UserInfo;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * 用户服务
+ */
+public interface UserService extends IService<User> {
+
+    /**
+     * 用户注册
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户 id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword);
+
+    /**
+     * 用户登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+    /**
+     * 用户登录（微信开放平台）
+     *
+     * @param wxOAuth2UserInfo 从微信获取的用户信息
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLoginByMpOpen(WxOAuth2UserInfo wxOAuth2UserInfo, HttpServletRequest request);
+
+    /**
+     * 获取当前登录用户
+     *
+     * @param request
+     * @return
+     */
+    User getLoginUser(HttpServletRequest request);
+
+    /**
+     * 获取当前登录用户（允许未登录）
+     *
+     * @param request
+     * @return
+     */
+    User getLoginUserPermitNull(HttpServletRequest request);
+
+    /**
+     * 是否为管理员
+     *
+     * @param request
+     * @return
+     */
+    boolean isAdmin(HttpServletRequest request);
+
+    /**
+     * 是否为管理员
+     *
+     * @param user
+     * @return
+     */
+    boolean isAdmin(User user);
+
+    /**
+     * 用户注销
+     *
+     * @param request
+     * @return
+     */
+    boolean userLogout(HttpServletRequest request);
+
+    /**
+     * 获取脱敏的已登录用户信息
+     *
+     * @return
+     */
+    LoginUserVO getLoginUserVO(User user);
+
+    /**
+     * 获取脱敏的用户信息
+     *
+     * @param user
+     * @return
+     */
+    UserVO getUserVO(User user);
+
+    /**
+     * 获取脱敏的用户信息
+     *
+     * @param userList
+     * @return
+     */
+    List<UserVO> getUserVO(List<User> userList);
+
+    /**
+     * 获取查询条件
+     *
+     * @param userQueryRequest
+     * @return
+     */
+    QueryWrapper<User> getQueryWrapper(UserQueryRequest userQueryRequest);
+
+}

Cuber-Wei/movie-system-backend

src/main/java/com/l0v3ch4n/oj/controller/PostFavourController.java
@@ -1,0 +1,107 @@
+package com.l0v3ch4n.oj.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.l0v3ch4n.oj.common.BaseResponse;
+import com.l0v3ch4n.oj.common.ErrorCode;
+import com.l0v3ch4n.oj.common.ResultUtils;
+import com.l0v3ch4n.oj.exception.BusinessException;
+import com.l0v3ch4n.oj.exception.ThrowUtils;
+import com.l0v3ch4n.oj.model.dto.post.PostQueryRequest;
+import com.l0v3ch4n.oj.model.dto.postfavour.PostFavourAddRequest;
+import com.l0v3ch4n.oj.model.dto.postfavour.PostFavourQueryRequest;
+import com.l0v3ch4n.oj.model.entity.Post;
+import com.l0v3ch4n.oj.model.entity.User;
+import com.l0v3ch4n.oj.model.vo.PostVO;
+import com.l0v3ch4n.oj.service.PostFavourService;
+import com.l0v3ch4n.oj.service.PostService;
+import com.l0v3ch4n.oj.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 帖子收藏接口
+ */
+@RestController
+@RequestMapping("/post_favour")
+@Slf4j
+public class PostFavourController {
+
+    @Resource
+    private PostFavourService postFavourService;
+
+    @Resource
+    private PostService postService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 收藏 / 取消收藏
+     *
+     * @param postFavourAddRequest
+     * @param request
+     * @return resultNum 收藏变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doPostFavour(@RequestBody PostFavourAddRequest postFavourAddRequest,
+                                              HttpServletRequest request) {
+        if (postFavourAddRequest == null || postFavourAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能操作
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postFavourAddRequest.getPostId();
+        int result = postFavourService.doPostFavour(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+    /**
+     * 获取我收藏的帖子列表
+     *
+     * @param postQueryRequest
+     * @param request
+     */
+    @PostMapping("/my/list/page")
+    public BaseResponse<Page<PostVO>> listMyFavourPostByPage(@RequestBody PostQueryRequest postQueryRequest,
+                                                             HttpServletRequest request) {
+        if (postQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User loginUser = userService.getLoginUser(request);
+        long current = postQueryRequest.getCurrent();
+        long size = postQueryRequest.getPageSize();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        Page<Post> postPage = postFavourService.listFavourPostByPage(new Page<>(current, size),
+                postService.getQueryWrapper(postQueryRequest), loginUser.getUserId());
+        return ResultUtils.success(postService.getPostVOPage(postPage, request));
+    }
+
+    /**
+     * 获取用户收藏的帖子列表
+     *
+     * @param postFavourQueryRequest
+     * @param request
+     */
+    @PostMapping("/list/page")
+    public BaseResponse<Page<PostVO>> listFavourPostByPage(@RequestBody PostFavourQueryRequest postFavourQueryRequest,
+                                                           HttpServletRequest request) {
+        if (postFavourQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        long current = postFavourQueryRequest.getCurrent();
+        long size = postFavourQueryRequest.getPageSize();
+        Long userId = postFavourQueryRequest.getUserId();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20 || userId == null, ErrorCode.PARAMS_ERROR);
+        Page<Post> postPage = postFavourService.listFavourPostByPage(new Page<>(current, size),
+                postService.getQueryWrapper(postFavourQueryRequest.getPostQueryRequest()), userId);
+        return ResultUtils.success(postService.getPostVOPage(postPage, request));
+    }
+}

src/main/java/com/l0v3ch4n/oj/model/dto/post/PostEsDTO.java
@@ -1,0 +1,109 @@
+package com.l0v3ch4n.oj.model.dto.post;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.json.JSONUtil;
+import com.l0v3ch4n.oj.model.entity.Post;
+import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 帖子 ES 包装类
+ **/
+// todo 取消注释开启 ES（须先配置 ES）
+//@Document(indexName = "post")
+@Data
+public class PostEsDTO implements Serializable {
+
+    private static final String DATE_TIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+
+    /**
+     * id
+     */
+    @Id
+    private Long postId;
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 内容
+     */
+    private String content;
+
+    /**
+     * 标签列表
+     */
+    private List<String> tag;
+
+    /**
+     * 创建用户 id
+     */
+    private Long userId;
+
+    /**
+     * 创建时间
+     */
+    @Field(index = false, store = true, type = FieldType.Date, format = {}, pattern = DATE_TIME_PATTERN)
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    @Field(index = false, store = true, type = FieldType.Date, format = {}, pattern = DATE_TIME_PATTERN)
+    private Date updateTime;
+
+    /**
+     * 是否删除
+     */
+    private Integer isDelete;
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 对象转包装类
+     *
+     * @param post
+     * @return
+     */
+    public static PostEsDTO objToDto(Post post) {
+        if (post == null) {
+            return null;
+        }
+        PostEsDTO postEsDTO = new PostEsDTO();
+        BeanUtils.copyProperties(post, postEsDTO);
+        String tagsStr = post.getTag();
+        if (StringUtils.isNotBlank(tagsStr)) {
+            postEsDTO.setTag(JSONUtil.toList(tagsStr, String.class));
+        }
+        return postEsDTO;
+    }
+
+    /**
+     * 包装类转对象
+     *
+     * @param postEsDTO
+     * @return
+     */
+    public static Post dtoToObj(PostEsDTO postEsDTO) {
+        if (postEsDTO == null) {
+            return null;
+        }
+        Post post = new Post();
+        BeanUtils.copyProperties(postEsDTO, post);
+        List<String> tagList = postEsDTO.getTag();
+        if (CollUtil.isNotEmpty(tagList)) {
+            post.setTag(JSONUtil.toJsonStr(tagList));
+        }
+        return post;
+    }
+}

src/main/java/com/l0v3ch4n/oj/service/UserService.java
@@ -1,0 +1,129 @@
+package com.l0v3ch4n.oj.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.l0v3ch4n.oj.model.dto.user.UserQueryRequest;
+import com.l0v3ch4n.oj.model.entity.User;
+import com.l0v3ch4n.oj.model.vo.LoginUserVO;
+import com.l0v3ch4n.oj.model.vo.UserVO;
+import me.chanjar.weixin.common.bean.WxOAuth2UserInfo;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * 用户服务
+ */
+public interface UserService extends IService<User> {
+
+    /**
+     * 用户注册
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户 id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword, String userPhone, String userMail, String verityCode);
+
+    /**
+     * 用户账号登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLoginWithAccount(String userAccount, String userPassword, HttpServletRequest request);
+
+    /**
+     * 用户验证码登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLoginWithVerity(String userAccount, String userPassword, HttpServletRequest request);
+
+    /**
+     * 用户登录（微信开放平台）
+     *
+     * @param wxOAuth2UserInfo 从微信获取的用户信息
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLoginByMpOpen(WxOAuth2UserInfo wxOAuth2UserInfo, HttpServletRequest request);
+
+    /**
+     * 获取当前登录用户
+     *
+     * @param request
+     * @return
+     */
+    User getLoginUser(HttpServletRequest request);
+
+    /**
+     * 获取当前登录用户（允许未登录）
+     *
+     * @param request
+     * @return
+     */
+    User getLoginUserPermitNull(HttpServletRequest request);
+
+    /**
+     * 是否为管理员
+     *
+     * @param request
+     * @return
+     */
+    boolean isAdmin(HttpServletRequest request);
+
+    /**
+     * 是否为管理员
+     *
+     * @param user
+     * @return
+     */
+    boolean isAdmin(User user);
+
+    /**
+     * 用户注销
+     *
+     * @param request
+     * @return
+     */
+    boolean userLogout(HttpServletRequest request);
+
+    /**
+     * 获取脱敏的已登录用户信息
+     *
+     * @return
+     */
+    LoginUserVO getLoginUserVO(User user);
+
+    /**
+     * 获取脱敏的用户信息
+     *
+     * @param user
+     * @return
+     */
+    UserVO getUserVO(User user);
+
+    /**
+     * 获取脱敏的用户信息
+     *
+     * @param userList
+     * @return
+     */
+    List<UserVO> getUserVO(List<User> userList);
+
+    /**
+     * 获取查询条件
+     *
+     * @param userQueryRequest
+     * @return
+     */
+    QueryWrapper<User> getQueryWrapper(UserQueryRequest userQueryRequest);
+
+}